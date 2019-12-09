40e4c1b8b6b741872d8b85f3f0189595319d7a32
Libor Gabaj <gabaj.libor@slsp.sk>
2019-12-09 13:41:31 +0100

    Initial commit.

---
+++ README.rst
@@ -0,0 +1,12 @@
+**********************
+Purpose of the package
+**********************
+
+The package ``gbj_iot`` contains just one module ``iot`` with constants and
+functions for handling devices of Internet of Things.
+
+- The package is treated as a git submodule and is usually involved into other
+  Python packages as a sub-package.
+
+- In either case the package can be installed individually as a standalone
+  Python package.
---
+++ setup.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""Setup function for the package."""
+
+from setuptools import setup
+
+setup(
+  name='gbj_iot',
+  version='1.0.0',
+  description='Python package for module iot.',
+  long_description='Module for general IoT handling.',
+  classifiers=[
+    'Development Status :: 4 - Beta',
+    'License :: OSI Approved :: MIT License',
+    'Programming Language :: Python :: 3.8',
+    'Topic :: System :: Hardware',
+  ],
+  keywords='common, system, fan',
+  url='http://github.com/mrkalePythonLib/gbj_iot',
+  author='Libor Gabaj',
+  author_email='libor.gabaj@gmail.com',
+  license='MIT',
+  packages=['gbj_iot'],
+  install_requires=[],
+  include_package_data=True,
+  zip_safe=False
+)
