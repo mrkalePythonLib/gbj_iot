005be2f8f14cbea1af082b4eb7e91e9f6746ab5d
Libor Gabaj <libor.gabaj@gmail.com>
2019-12-22 00:58:47 +0100

    Added dataclass and further enumerations.

--- gbj_iot/iot.py
+++ gbj_iot/iot.py
@@ -13,7 +13,8 @@ __email__ = 'libor.gabaj@gmail.com'
 # Standard library modules
 import logging
 from abc import ABC, abstractmethod
-from enum import Enum
+from enum import Enum, auto
+from dataclasses import dataclass
 
 
 ###############################################################################
@@ -42,10 +43,31 @@ class Category(Enum):
     DATA = 'data'
 
 
+class Parameter(Enum):
+    """Enumeration of expected MQTT topic parameters."""
+    ...
+
+
+class Measure(Enum):
+    """Enumeration of possible MQTT topi measures."""
+    VALUE = 'val'
+    MINIMUM = 'min'
+    MAXIMUM = 'max'
+    AVERAGE = 'avg'
+
+
 ###############################################################################
 # IoT core
 ###############################################################################
-class Plugin(object):
+@dataclass
+class PluginParam:
+    """Plugin parameter record definition."""
+    parameter: str
+    measure: Measure = None
+    value: any = None
+
+
+class Plugin(ABC):
     """General processing for IoT devices."""
 
     # Predefined configuration file sections related to MQTT
@@ -55,6 +77,8 @@ class Plugin(object):
     def __init__(self):
         """Create the class instance - constructor."""
         self.mqtt_client = None
+        # Private attributes
+        self._params: [PluginParam] = []
         # Logging
         self._logger = logging.getLogger(' '.join([__name__, __version__]))
 
@@ -101,11 +125,61 @@ class Plugin(object):
             self._logger.error(errmsg)
             raise ValueError(errmsg)
 
+    def check_parameter(self, parameter: Parameter) -> str:
+        """Check validity of the parameter enumeration member and return
+        its value.
+
+        Returns
+        -------
+            Parameter name for MQTT topic.
+
+        Raises
+        -------
+        ValueError
+            Input string is not an enumeration key.
+
+        """
+        try:
+            if isinstance(parameter, Parameter):
+                parameter = parameter.value
+            else:
+                parameter = Parameter[parameter].value
+            return parameter
+        except KeyError:
+            errmsg = f'Unknown MQTT topic {parameter=}'
+            self._logger.error(errmsg)
+            raise ValueError(errmsg)
+
+    def check_measure(self, measure: Measure) -> str:
+        """Check validity of the measure enumeration member and return
+        its value.
+
+        Returns
+        -------
+            Measure name for MQTT topic.
+
+        Raises
+        -------
+        ValueError
+            Input string is not an enumeration key.
+
+        """
+        try:
+            if isinstance(measure, Measure):
+                measure = measure.value
+            else:
+                measure = Measure[measure].value
+            return measure
+        except KeyError:
+            errmsg = f'Unknown MQTT topic {measure=}'
+            self._logger.error(errmsg)
+            raise ValueError(errmsg)
+
     def get_topic(
         self,
         category: Category,
         parameter: str = None,
-        measure: str = None) -> str:
+        measure: Measure = None) -> str:
         """Compose MQTT topic name from prescribed topic parts.
 
         Arguments
@@ -117,7 +191,7 @@ class Plugin(object):
             transmitted by the MQTT topic. It is usually a name of a physical
             unit or an operational parameter.
         measure
-            Optional and arbitrary name of an aspect related to tranmitted
+            Optional enumerated measure of an aspect related to tranmitted
             value. It is usually a statistical measure like minimum, maximum,
             current value, etc.
 
@@ -130,17 +204,19 @@ class Plugin(object):
         topic.append(self.check_category(category))
         # Optional topic parts
         if parameter:
-            topic.append(parameter)
+            topic.append(self.check_parameter(parameter))
         if measure:
-            topic.append(measure)
+            topic.append(self.check_measure(measure))
         # Compose topic
         topic_name = self.TOPIC_SEP.join(topic)
         return topic_name
 
+    @abstractmethod
     def begin(self):
         """Actions at starting IoT application."""
         self._logger.debug(f'Plugin "{self.id}" started')
 
+    @abstractmethod
     def finish(self):
         """Actions at finishing IoT application."""
         self._logger.debug(f'Plugin "{self.id}" stopped')
@@ -151,3 +227,92 @@ class Plugin(object):
         typical for the plugin.
         """
         ...
+
+###############################################################################
+# Parameters actions
+###############################################################################
+    def _find_param(self, parameter: Parameter, measure: Measure) -> int:
+        """Find parameter in the device's parameters list.
+
+        Arguments
+        ---------
+        parameter
+            Enumerated parameter to be found.
+        measure
+            Enumerated measure to be found.
+
+        Returns
+        -------
+            Index of found parameter to the list of parameters.
+
+        Raises
+        -------
+        ValueError
+            Parameter not found or given unknown measure.
+
+        """
+        if parameter is not None:
+            _ = self.check_parameter(parameter)
+        if measure is not None:
+            _ = self.check_measure(measure)
+        for index, param in enumerate(self._params):
+            if parameter == param.parameter \
+            and (measure is None or measure == param.measure):
+                return index
+        raise ValueError
+
+    def get_param(self, parameter: Parameter, measure: Measure) -> any:
+        """Get parameter value from the device's parameters list.
+
+        Arguments
+        ---------
+        parameter
+            Enumerated parameter to be found.
+        measure
+            Enumerated measure to be found.
+
+        Returns
+        -------
+            Value of a parameter or None.
+
+        Raises
+        -------
+        ValueError
+            Parameter not found or given unknown measure.
+
+        """
+        result = None
+        try:
+            index = self._find_param(parameter, measure)
+        except ValueError as errmsg:
+            self._logger.error(errmsg)
+        else:
+            result = self._params[index].value
+        return result
+
+    def set_param(self, value: any, parameter: Parameter, measure: Measure):
+        """Set or update parameter with given value.
+
+        Arguments
+        ---------
+        value
+            Value of a parameter to be saved or updated.
+        parameter
+            Enumerated parameter to be created or updated.
+        measure
+            Enumerated measure to be found.
+
+        Raises
+        -------
+        ValueError
+            Parameter not found or given unknown measure.
+
+        """
+        try:
+            index = self._find_param(parameter, measure)
+            self._params[index].value = value
+        except ValueError:
+            param = PluginParam(parameter, measure, value)
+            self._params.append(param)
+        except Exception as errmsg:
+            self._logger.error(errmsg)
